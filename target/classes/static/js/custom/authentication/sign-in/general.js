9238c2c77de9c44f50842d3a56b124f927f633c2
info@asran.ir <info@asran.ir>
2023-03-14 11:42:12 +0330

    add dashboard page

---
+++ target/classes/static/js/custom/authentication/sign-in/general.js
@@ -0,0 +1,93 @@
+"use strict";
+
+var KTSigninGeneral = function () {
+    // Elements
+    var form;
+    var submitButton;
+    var validator;
+
+    // Handle form
+    var handleForm = function (e) {
+        // Init form validation rules. For more info check the FormValidation plugin's official documentation:https://formvalidation.io/
+        validator = FormValidation.formValidation(
+            form,
+            {
+                fields: {
+                    'username': {
+                        validators: {
+                            notEmpty: {
+                                message: 'ورود نام کاربری الزامی است'
+                            }
+                        }
+                    },
+                    'password': {
+                        validators: {
+                            notEmpty: {
+                                message: 'ورود کلمه عبور الزامی است'
+                            }
+                        }
+                    },'name': {
+                        validators: {
+                            notEmpty: {
+                                message: 'ورود نام الزامی است'
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    trigger: new FormValidation.plugins.Trigger(),
+                    bootstrap: new FormValidation.plugins.Bootstrap5({
+                        rowSelector: '.fv-row'
+                    })
+                }
+            }
+        );
+
+        // Handle form submit
+        submitButton.addEventListener('click', function (e) {
+            // Prevent button default action
+            e.preventDefault();
+
+            // Validate form
+            validator.validate().then(function (status) {
+                if (status == 'Valid') {
+                    // Show loading indication
+                    submitButton.setAttribute('data-kt-indicator', 'on');
+                    // Disable button to avoid multiple click
+                    submitButton.disabled = true;
+
+                    $.ajax({
+                        type: "POST"
+                        , url: "login"
+                        , data: "username=" + $('#username').val() + "&password=" + $('#password').val()
+                    });
+                    // Simulate ajax request
+                    setTimeout(function () {
+                        // Hide loading indication
+                        submitButton.removeAttribute('data-kt-indicator');
+
+                        // Enable button
+                        submitButton.disabled = false;
+                    }, 2000)
+                } else {
+                }
+            });
+        });
+    }
+
+    // Public functions
+    return {
+        // Initialization
+        init: function () {
+            form = document.querySelector('#kt_sign_in_form');
+            submitButton = document.querySelector('#kt_sign_in_submit');
+
+            handleForm();
+        }
+    };
+}();
+
+// On document ready
+KTUtil.onDOMContentLoaded(function () {
+    KTSigninGeneral.init();
+});
