2b5ca29e9363726434249ba5c4641c3780f1305e
info@asran.ir <info@asran.ir>
2023-03-19 13:34:47 +0330

    set loadChapter

---
+++ src/main/resources/static/js/custom/custom.js
@@ -0,0 +1,18 @@
+/**
+ * loadChapter
+ * @param chapter
+ */
+function loadChapter(chapter) {
+    //go to chapter
+    $('.loadChapter').addClass('d-none');
+    $('#' + chapter).removeClass('d-none');
+
+    //add active class
+}
+
+$(document).ready(function () {
+    var crumbs = {
+        "ch1": "home.html",
+        "ch2": "news.html"
+    };
+});
